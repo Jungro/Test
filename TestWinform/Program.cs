a7c7bb310fe8a5cd4cd82478d0ffb50c6846a5ca
Jungro1 <jungro425@naver.com>
2014-01-16 05:11:33 +0900

    Update Program.cs
    3번째 수정

--- TestWinform/Program.cs
+++ TestWinform/Program.cs
@@ -15,7 +15,7 @@ namespace TestWinform
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());    
+            Application.Run(new Form1());      
         }
     }
 }
